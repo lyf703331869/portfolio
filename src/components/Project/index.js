01b27211f201d74c05001f7c09f44360f44730ad
Yafei Liu <yafeiliu@yafeis-air.mynetworksettings.com>
2023-07-13 18:11:06 -0400

    Added project page

---
+++ src/components/Project/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { removeHyphensAndCapitalize } from "../../utils/helpers";
+
+export default function Project({ project }) {
+  const { name, repo, link, description } = project;
+
+  return (
+    <div className="project" key={name}>
+      <img
+        src={require(`../../assets/projects/${name}.jpg`)}
+        alt={removeHyphensAndCapitalize(name)}
+        className="project-bg"
+      />
+      <div className="project-text">
+        <h3>
+          <a href={link}>{removeHyphensAndCapitalize(name)}</a>{" "}
+          <a href={repo}>
+            <i className="fab fa-github"></i>
+          </a>
+        </h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
