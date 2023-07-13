78838ef14e0106a4fcafd82218269b291e56d8a6
Yafei Liu <yafeiliu@yafeis-air.mynetworksettings.com>
2023-07-13 17:33:01 -0400

    Added header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Header({ children }) {
-  return <></>;
+  return (
+    <header className="flex-row space-between px-1">
+      <h1>Yafei Liu</h1>
+      {children}
+    </header>
+  );
 }
