4237eb5f5bafd25d907b3a791262bac7585722ed
Yafei Liu <yafeiliu@yafeis-air.mynetworksettings.com>
2023-07-21 19:08:20 -0400

    Debugged the Portfolio showing

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,81 +1,123 @@
-import React, { useState } from "react";
-import { validateEmail } from "../../utils/helpers";
+import React from "react";
+
+const styles = {
+  maps: { border: 0, width: "100%", height: "270px" },
+};
 
 export default function Contact() {
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const [errorMessage, setErrorMessage] = useState("");
-  const { name, email, message } = formState;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!errorMessage) {
-      console.log("Submit Form", formState);
-    }
-  };
-
-  const handleChange = (e) => {
-    if (e.target.name === "email") {
-      const isValid = validateEmail(e.target.value);
-      if (!isValid) {
-        setErrorMessage("Your email is invalid.");
-      } else {
-        setErrorMessage("");
-      }
-    } else {
-      if (!e.target.value.length) {
-        setErrorMessage(`${e.target.name} is required.`);
-      } else {
-        setErrorMessage("");
-      }
-    }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-      console.log("Handle Form", formState);
-    }
-  };
-
   return (
-    <section>
-      <form id="contact-form" onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name:</label>
-          <input
-            type="text"
-            name="name"
-            defaultValue={name}
-            onBlur={handleChange}
-          />
+    <section id="contact" className="contact">
+      <div
+        className="container"
+        data-aos="fade-up"
+        data-aos-offset="200"
+        data-aos-delay="50"
+        data-aos-duration="1200"
+        data-aos-once="true"
+      >
+        <div className="section-title">
+          <h2>Contact</h2>
+          <p>
+            Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
+            aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
+            quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
+            fugiat sit in iste officiis commodi quidem hic quas.
+          </p>
         </div>
         <div>
-          <label htmlFor="email">Email address:</label>
-          <input
-            type="email"
-            name="email"
-            defaultValue={email}
-            onBlur={handleChange}
+          <iframe
+            style={styles.maps}
+            me
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d387194.06234425615!2d-74.30933206527233!3d40.69701927394441!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY!5e0!3m2!1sen!2sus!4v1689972276130!5m2!1sen!2sus"
+            frameborder="0"
+            allowfullscreen
           />
         </div>
-        <div>
-          <label htmlFor="message">Message:</label>
-          <textarea
-            name="message"
-            rows="5"
-            defaultValue={message}
-            onBlur={handleChange}
-          />
-        </div>
-        {errorMessage && (
-          <div>
-            <p className="error-text">{errorMessage}</p>
+        <div className="row mt-5">
+          <div className="col-lg-4">
+            <div className="info">
+              <div className="address">
+                <i className="bi bi-geo-alt"></i>
+                <h4>Location:</h4>
+                <p>New York, NY 11375</p>
+              </div>
+
+              <div className="email">
+                <i className="bi bi-envelope"></i>
+                <h4>Email:</h4>
+                <p>yafeiliunyc@outlook.com</p>
+              </div>
+
+              <div className="phone">
+                <i className="bi bi-phone"></i>
+                <h4>Call:</h4>
+                <p>+1 (929) 204-6236</p>
+              </div>
+            </div>
           </div>
-        )}
-        <button type="submit">Submit</button>
-      </form>
+
+          <div className="col-lg-8 mt-5 mt-lg-0">
+            <form
+              action="forms/contact.php"
+              method="post"
+              role="form"
+              className="php-email-form"
+            >
+              <div className="row">
+                <div className="col-md-6 form-group">
+                  <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    id="name"
+                    placeholder="Your Name"
+                    required
+                  />
+                </div>
+                <div className="col-md-6 form-group mt-3 mt-md-0">
+                  <input
+                    type="email"
+                    className="form-control"
+                    name="email"
+                    id="email"
+                    placeholder="Your Email"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="form-group mt-3">
+                <input
+                  type="text"
+                  className="form-control"
+                  name="subject"
+                  id="subject"
+                  placeholder="Subject"
+                  required
+                />
+              </div>
+              <div className="form-group mt-3">
+                <textarea
+                  className="form-control"
+                  name="message"
+                  rows="5"
+                  placeholder="Message"
+                  required
+                ></textarea>
+              </div>
+              <div className="my-3">
+                <div className="loading">Loading</div>
+                <div className="error-message"></div>
+                <div className="sent-message">
+                  Your message has been sent. Thank you!
+                </div>
+              </div>
+              <div className="text-center">
+                <button type="submit">Send Message</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from "react";
 import Project from "../Project";
-// import Isotope from "isotope-layout";
+import Isotope from "isotope-layout";
 
 export default function Portfolio() {
-  // const [filterKey, setFilterKey] = useState("");
-  // const [isotope, setIsotope] = useState(null);
-  // let portfolioFilters = document.querySelectorAll("#portfolio-flters li");
+  const [filterKey, setFilterKey] = useState("*");
+  const [isotope, setIsotope] = useState(null);
 
-  // useEffect(() => {
-  //   setIsotope(new Isotope(".portfolio-container"));
-  // }, []);
+  useEffect(() => {
+    setIsotope(new Isotope(".portfolio-container"));
+  }, []);
 
-  // useEffect(() => {
-  //   if (isotope) {
-  //     isotope.arrange({ filter: `${filterKey}` });
-  //   }
-  // }, [filterKey]);
+  useEffect(() => {
+    if (isotope) {
+      isotope.arrange({ filter: `${filterKey}` });
+    }
+  }, [filterKey]);
 
-  // const handleFilterKeyChange = (e) => {
-  //   portfolioFilters.forEach(function (el) {
-  //     el.classList.remove("filter-active");
-  //   });
-  //   e.target.classList.add("filter-active");
-  //   setFilterKey(e.target.getAttribute("data-filter"));
-  // };
+  const handleFilterKeyChange = (e) => {
+    let portfolioFilters = document.querySelectorAll("#portfolio-flters li");
+    portfolioFilters.forEach(function (el) {
+      el.classList.remove("filter-active");
+    });
+    e.target.classList.add("filter-active");
+    setFilterKey(e.target.getAttribute("data-filter"));
+  };
 
   const projects = [
     {
@@ -63,29 +63,20 @@ export default function Portfolio() {
       >
         <div className="section-title">
           <h2>Portfolio</h2>
-          <p>
-            Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex
-            aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos
-            quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-            fugiat sit in iste officiis commodi quidem hic quas.
-          </p>
+          <p>Below please feel free to see some of my recent projects.</p>
         </div>
 
-        {/* <div className="row" data-aos="fade-up" data-aos-delay="100">
+        <div className="row" data-aos="fade-up" data-aos-delay="100">
           <div className="col-lg-12 d-flex justify-content-center">
-            <ul id="portfolio-flters">
-              <li data-filter="*" onClick={handleFilterKeyChange}>
+            <ul id="portfolio-flters" onClick={handleFilterKeyChange}>
+              <li data-filter="*" className="filter-active">
                 All
               </li>
-              <li data-filter=".filter-app" onClick={handleFilterKeyChange}>
-                App
-              </li>
-              <li data-filter=".filter-web" onClick={handleFilterKeyChange}>
-                Web
-              </li>
+              <li data-filter=".filter-app">App</li>
+              <li data-filter=".filter-web">Web</li>
             </ul>
           </div>
-        </div> */}
+        </div>
 
         <div
           className="row portfolio-container"
