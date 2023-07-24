605380b848bad050967c50499d0612e02103829b
Yafei Liu <yafeiliu@yafeis-air.mynetworksettings.com>
2023-07-24 16:32:53 -0400

    Updated the hero

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -18,7 +18,7 @@ export default function Home({ setCurrentPage }) {
         className="container d-flex flex-column align-items-center"
         data-aos="zoom-in"
         data-aos-delay="100"
-        data-aos-duration="5000"
+        data-aos-duration="1000"
       >
         <h1 style={styles.font}>Yafei Liu</h1>
         <h2 style={styles.paragraph}>
